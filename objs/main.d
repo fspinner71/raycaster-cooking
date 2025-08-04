6cde67c13cc7135e8240d89e175833000e0439c8
fspinner71 <nathans.fs71@gmail.com>
2025-08-04 02:52:19 -0500

    made cmakelists and added dll files

--- objs/main.d
+++ objs/main.d
@@ -1,39 +1,40 @@
 objs/main.o: src/main.cpp src/game.h include/SDL3/SDL.h \
-  include/SDL3/SDL_stdinc.h include/SDL3/SDL_platform_defines.h \
-  include/SDL3/SDL_begin_code.h include/SDL3/SDL_close_code.h \
-  include/SDL3/SDL_assert.h include/SDL3/SDL_asyncio.h \
-  include/SDL3/SDL_atomic.h include/SDL3/SDL_audio.h \
-  include/SDL3/SDL_endian.h include/SDL3/SDL_error.h \
-  include/SDL3/SDL_mutex.h include/SDL3/SDL_thread.h \
-  include/SDL3/SDL_properties.h include/SDL3/SDL_iostream.h \
-  include/SDL3/SDL_bits.h include/SDL3/SDL_blendmode.h \
-  include/SDL3/SDL_camera.h include/SDL3/SDL_pixels.h \
-  include/SDL3/SDL_surface.h include/SDL3/SDL_rect.h \
-  include/SDL3/SDL_clipboard.h include/SDL3/SDL_cpuinfo.h \
-  include/SDL3/SDL_dialog.h include/SDL3/SDL_video.h \
-  include/SDL3/SDL_events.h include/SDL3/SDL_gamepad.h \
-  include/SDL3/SDL_guid.h include/SDL3/SDL_joystick.h \
-  include/SDL3/SDL_power.h include/SDL3/SDL_sensor.h \
-  include/SDL3/SDL_keyboard.h include/SDL3/SDL_keycode.h \
-  include/SDL3/SDL_scancode.h include/SDL3/SDL_mouse.h \
-  include/SDL3/SDL_pen.h include/SDL3/SDL_touch.h \
-  include/SDL3/SDL_filesystem.h include/SDL3/SDL_gpu.h \
-  include/SDL3/SDL_haptic.h include/SDL3/SDL_hidapi.h \
-  include/SDL3/SDL_hints.h include/SDL3/SDL_init.h \
-  include/SDL3/SDL_loadso.h include/SDL3/SDL_locale.h \
-  include/SDL3/SDL_log.h include/SDL3/SDL_messagebox.h \
-  include/SDL3/SDL_metal.h include/SDL3/SDL_misc.h \
-  include/SDL3/SDL_platform.h include/SDL3/SDL_process.h \
-  include/SDL3/SDL_render.h include/SDL3/SDL_storage.h \
-  include/SDL3/SDL_system.h include/SDL3/SDL_time.h \
-  include/SDL3/SDL_timer.h include/SDL3/SDL_tray.h \
-  include/SDL3/SDL_version.h include/SDL3/SDL_oldnames.h \
-  include/SDL3/SDL_main.h include/SDL3_ttf/SDL_textengine.h \
-  include/SDL3_ttf/SDL_ttf.h src/assetmanager.h \
-  include/SDL3_image/SDL_image.h include/SDL3_mixer/SDL_mixer.h \
-  src/map.h src/player.h src/math.h src/scene.h src/npc.h src/sprite.h \
-  src/interaction.h src/stove.h src/fridge.h src/canvas.h src/frame.h \
-  src/text.h src/raycast.h src/camera.h src/strip.h
+ include/SDL3/SDL_stdinc.h include/SDL3/SDL_platform_defines.h \
+ include/SDL3/SDL_begin_code.h include/SDL3/SDL_close_code.h \
+ include/SDL3/SDL_assert.h include/SDL3/SDL_asyncio.h \
+ include/SDL3/SDL_atomic.h include/SDL3/SDL_audio.h \
+ include/SDL3/SDL_endian.h include/SDL3/SDL_error.h \
+ include/SDL3/SDL_mutex.h include/SDL3/SDL_thread.h \
+ include/SDL3/SDL_properties.h include/SDL3/SDL_iostream.h \
+ include/SDL3/SDL_bits.h include/SDL3/SDL_blendmode.h \
+ include/SDL3/SDL_camera.h include/SDL3/SDL_pixels.h \
+ include/SDL3/SDL_surface.h include/SDL3/SDL_rect.h \
+ include/SDL3/SDL_clipboard.h include/SDL3/SDL_cpuinfo.h \
+ include/SDL3/SDL_dialog.h include/SDL3/SDL_video.h \
+ include/SDL3/SDL_events.h include/SDL3/SDL_gamepad.h \
+ include/SDL3/SDL_guid.h include/SDL3/SDL_joystick.h \
+ include/SDL3/SDL_power.h include/SDL3/SDL_sensor.h \
+ include/SDL3/SDL_keyboard.h include/SDL3/SDL_keycode.h \
+ include/SDL3/SDL_scancode.h include/SDL3/SDL_mouse.h \
+ include/SDL3/SDL_pen.h include/SDL3/SDL_touch.h \
+ include/SDL3/SDL_filesystem.h include/SDL3/SDL_gpu.h \
+ include/SDL3/SDL_haptic.h include/SDL3/SDL_hidapi.h \
+ include/SDL3/SDL_hints.h include/SDL3/SDL_init.h \
+ include/SDL3/SDL_loadso.h include/SDL3/SDL_locale.h \
+ include/SDL3/SDL_log.h include/SDL3/SDL_messagebox.h \
+ include/SDL3/SDL_metal.h include/SDL3/SDL_misc.h \
+ include/SDL3/SDL_platform.h include/SDL3/SDL_process.h \
+ include/SDL3/SDL_render.h include/SDL3/SDL_storage.h \
+ include/SDL3/SDL_system.h include/SDL3/SDL_time.h \
+ include/SDL3/SDL_timer.h include/SDL3/SDL_tray.h \
+ include/SDL3/SDL_version.h include/SDL3/SDL_oldnames.h \
+ include/SDL3/SDL_main.h include/SDL3/SDL_main_impl.h \
+ include/SDL3_ttf/SDL_textengine.h include/SDL3_ttf/SDL_ttf.h \
+ src/assetmanager.h include/SDL3_image/SDL_image.h \
+ include/SDL3_mixer/SDL_mixer.h src/map.h src/player.h src/math.h \
+ src/scene.h src/npc.h src/sprite.h src/interaction.h src/stove.h \
+ src/fridge.h src/canvas.h src/frame.h src/text.h src/raycast.h \
+ src/camera.h src/strip.h
 src/game.h:
 include/SDL3/SDL.h:
 include/SDL3/SDL_stdinc.h:
@@ -95,6 +96,7 @@ include/SDL3/SDL_tray.h:
 include/SDL3/SDL_version.h:
 include/SDL3/SDL_oldnames.h:
 include/SDL3/SDL_main.h:
+include/SDL3/SDL_main_impl.h:
 include/SDL3_ttf/SDL_textengine.h:
 include/SDL3_ttf/SDL_ttf.h:
 src/assetmanager.h:
